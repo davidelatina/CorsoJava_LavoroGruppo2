ce359702325217b53597cb0adbcc009b46433007
GiulianaSalinardi <giulianasalinardi@gmail.com>
2025-07-04 16:48:53 +0200

    Sottoclassi corretto

--- Sottoclassi.java
+++ Sottoclassi.java
@@ -77,7 +77,7 @@ this.formato = formato;
 public String getFormato() {
     return formato;
 }
-public void setFormatoa(String formato) {
+public void setFormato(String formato) {
     this.formato = formato;
 }
 
